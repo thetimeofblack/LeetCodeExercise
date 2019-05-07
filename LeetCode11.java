bbf4cf7d41d5bd2a47ea96f79fa8ee899dc54d99
thetimeofblack <1191401005@qq.com>
2019-05-07 13:12:03 +0200

    LeetCode11

---
+++ LeetCode11.java
@@ -0,0 +1,14 @@
+class LeetCode11{
+	public static int maxArea(int[] height) {
+        int [] ma = new int[height.length]; 
+    }
+	
+	public static void main(String[] args){
+		int[] h1 = {1,2,3,4,5}; 
+		int[] h2 = {1,8,6,2,5,4,8,3,7}	; 
+		System.out.println("The Result of Test 1 is "+maxArea(h1)); 
+		System.out.println("The Result of Test 2 is "+maxArea(h2)); 
+	
+	}
+	
+}
