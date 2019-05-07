af464d61bd8af3791d69f496ea8c6110930bb837
thetimeofblack <1191401005@qq.com>
2019-05-07 12:32:46 +0200

    all

---
+++ LeetCode12.java
@@ -0,0 +1,8 @@
+//LeetCode 12
+class LeetCode12 {
+
+	public static void main(String[] args){
+		System.out.println("The result of Test 1 is "); 
+	}
+
+}
---
+++ LeetCode7.java
@@ -0,0 +1,14 @@
+class LeetCode7{
+	public static int reverse(int x) {
+			if(x==Integer.MIN_VALUE) return 0 ; 
+			
+    }
+	public static void main(String[] args){
+		int n1 = 234; 
+		int n2 = -123; 
+		int n3 = 120 ; 
+		
+		
+	}
+	
+}
