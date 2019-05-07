3c25f02dd29b39e4ef371233bc857c6359ffa85e
thetimeofblack <1191401005@qq.com>
2019-05-07 15:49:05 +0800

    LeetCode6

---
+++ LeetCode7.java
@@ -0,0 +1,15 @@
+class LeetCode7{
+	public int reverse(int x) {
+        
+    }
+	
+	public static void main(String[] args){
+		
+		int x1 = 123 ; 
+		int x2 = -321; 
+		int x3 = 120 ; 
+		System.out.println("The result of Test 1 is "+ reverse(x1)); 
+		System.out.println("The result of Test 1 is "+ reverse(x2));
+		System.out.println("The result of Test 1 is "+ reverse(x3));
+	}
+}
