1d97adef92783cedb3a07549a629a01fc83f93fa
thetimeofblack <1191401005@qq.com>
2019-05-07 14:21:03 +0200

    LeetCode11

--- LeetCode11.java
+++ LeetCode11.java
@@ -1,6 +1,34 @@
+/*
+
+Runtime: 697 ms, faster than 5.04% of Java online submissions for Container With Most Water.
+Memory Usage: 37.7 MB, less than 95.02% of Java online submissions for Container With Most Water.
+Next challenges:
+*/
+
 class LeetCode11{
 	public static int maxArea(int[] height) {
-        int [] ma = new int[height.length]; 
+		int len = height.length - 1; 
+		int result = 0 ; 
+		while(len>=1){
+			int tempma= 0 ;  
+			
+			for(int i= 0 ;(i+len)<height.length;i++){
+				
+				if((i+len)>height.length-1) break; 
+				int temp = 0; 
+				if(height[i]>height[i+len]){
+					
+					temp = len*height[i+len]; 
+				}else{
+					temp = len*height[i]; 
+				}
+				if(temp>=tempma) tempma = temp ; 
+			
+			} 
+			if(result<=tempma) result = tempma;  
+			len--;
+		}
+		return result ; 
     }
 	
 	public static void main(String[] args){
