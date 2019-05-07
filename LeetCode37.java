7e3114e52a9cefe0fcb88e04afccb364a7e1b7e6
thetimeofblack <1191401005@qq.com>
2019-05-08 00:55:10 +0200

    It is difficult to judge the sudoku rule

--- LeetCode37.java
+++ LeetCode37.java
@@ -1,16 +1,49 @@
 class LeetCode37{
 	public static void solveSudoku(char[][] board){
-		for(int i=0 ; i<board[0].length(); i++){
-			for(int j=0 ;j<board.length(); j++){
-				if()
-			}
-			
-		}
-		
+			solve(board,0); 
 	}
 	
+	public static boolean solve(char[][] board, int position){
+		if(position==81) return true; 
+		int row = position/9 ; 
+		int col = position%9 ; 
+		if(board[row][col]=='.'){
+			for(int i = 1 ; i<=9 ;i++){
+				board[row][col] =(char) (i+48); 
+				if(validate(board,position)){
+					if(solve(board,position+1)) {
+					    System.out.println("position "+position+"character "+board[row][col]);
+				
+						return true; 
+					}//System.out.println("position "+position+"character "+board[row][col]);
+				}
+				board[row][col] = '.'; 
+				
+			}
+		}else{
+			if(solve(board,position+1)) return true;  
+		}
+		return false;
+	}
+	
+	public static boolean validate(char[][] board , int position){
+			int row = position/9; 
+			int col = position%9;
+			int gridx = row/3*3; 
+			int gridy = col/3*3; 
+			for(int i= 0 ;i<9 ;i++){
+				if(i!=col&&board[row][i]==board[row][col]) return false;
+				if(i!=row&&board[i][col]==board[row][col]) return false;
+				int x = i/3; 
+				int y = i%3;
+				x += gridx ; 
+				y += gridy ; 
+				if((row!=x)&&(col!=y)&&(board[row][col]==board[x][y])) return false;
+			}
+			return true; 
+	}
 	public static void main(String[] args){
-		char[][] b1 = {{'5','3','.','.','7','.','.','.','.'}
+		char[][] b1 = {{'5','3','.','.','7','.','.','.','.'},
 					   {'6','.','.','1','9','5','.','.','.'},
 					   {'.','9','8','.','.','.','.','6','.'},
 					   {'8','.','.','.','6','.','.','.','3'},
@@ -19,8 +52,14 @@ class LeetCode37{
 					   {'.','6','.','.','.','.','2','8','1'},
 					   {'.','.','.','4','1','9','.','.','5'},
 					   {'.','.','.','.','8','.','.','7','9'}
-					  }
-		System.out.println("The Result of Test 1 is "+ solveSudoku(b1)); 
-	}
+					  };
+		solveSudoku(b1);
+		for(int i= 0 ;i<9 ;i++){
+			for(int j= 0 ; j<9 ;j++){
+				System.out.print(b1[i][j]+" "); 
+			}
+			System.out.println(); 
+		}
 	
+}
 } 
