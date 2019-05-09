9e750c3b0f3e1b44834efd89489d976caf70262a
thetimeofblack <1191401005@qq.com>
2019-05-10 00:31:06 +0200

    LeetCode17 AC

--- LeetCode17.java
+++ LeetCode17.java
@@ -1,4 +1,8 @@
-
+/*
+Runtime: 32 ms, faster than 8.05% of Java online submissions for Letter Combinations of a Phone Number.
+Memory Usage: 35.3 MB, less than 89.31% of Java online submissions for Letter Combinations of a Phone Number.
+Next challenges:
+*/
 import java.util.*; 
 class LeetCode17{
 	public static List<String> letterCombinations(String digits){
@@ -13,18 +17,36 @@ class LeetCode17{
 		map.put('7',Arrays.asList('p','q','r','s')); 
 		map.put('8',Arrays.asList('t','u','v'));
 		map.put('9',Arrays.asList('w','x','y','z'));
-		result.add("hello"); 
+		if(digits.isEmpty()) return result; 
+		solve(digits, "",map , 0, result);
 		return result; 
 	}
 	
-	public static void solve(String digits,HashMap<Character,List<Character>> map, int position){
+	public static void solve(String digits,String currentstring, HashMap<Character,List<Character>> map, int position,List<String> result){
+		char ch = digits.charAt(position);
+		List<Character> chlist = map.get(ch); 
+		if(position==digits.length()-1) {
+			chlist.forEach((letter)->{
+				result.add(currentstring+letter);
+			});
+		}else{
 		
+		chlist.forEach((letter)->{
+			solve(digits,currentstring+letter,map,position+1,result);  
+		}); 
+		}
 	}
 	
 	public static void main(String[] args){
 		List<String> list = new LinkedList<String>(); 
-		String d1 = "23"; 
+		String d1 = "23";
+		String d2 = "22333"; 
+		
 		list = letterCombinations(d1); 
+		for(int i= 0 ;i<list.size(); i++){
+			System.out.println(list.get(i)); 
+		}
+		list = letterCombinations(d2); 
 		for(int i= 0 ;i<list.size(); i++){
 			System.out.println(list.get(i)); 
 		}
