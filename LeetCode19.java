f6a7f3e3348d23c7a96a860a31098727e689213e
thetimeofblack <1191401005@qq.com>
2019-05-12 10:03:18 +0200

    100% for leetcode19

--- LeetCode19.java
+++ LeetCode19.java
@@ -1,13 +1,29 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Nth Node From End of List.
+Memory Usage: 34 MB, less than 100.00% of Java online submissions for Remove Nth Node From End of List.
+*/
+
 class LeetCode19{
 	public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head ; 
-		ListNode slow = head ; 
-		while(cnode.next!=null){
-			fast = fast.next; 
-			if(n==0) slow = slow.next; 
-			n--; 
+        ListNode chead =head; 
+		ListNode fast = head ; 
+		ListNode slow = head ;
+		int i = 0 ;
+		
+		if(head.next==null && n==1) {
+			return null; 
 		}
 		
+		while(fast.next!=null){
+			fast = fast.next; 
+			if(i>=n) slow = slow.next; 
+			i++;  
+		}
+		if(i<n) return slow.next;
+		ListNode next = slow.next.next;
+		slow.next = null; 
+		slow.next = next; 
+		return chead ;         
 		
     }
 	public static void main(String[] args){
@@ -16,8 +32,15 @@ class LeetCode19{
 		for(int i= 1 ;i < 10 ;i++){
 			ListNode node = new ListNode(i); 
 			cnode.next = node; 
-			cnode = head.next ;
+			cnode = cnode.next ;
 		}
+		removeNthFromEnd(head,4); 
+		ListNode index = head; 
+		while(index.next!=null){
+			System.out.println(index.val); 
+			index = index.next ;
+		}
+		System.out.println(index.val); 
 	}
 }
 
