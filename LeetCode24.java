04994f96243d27c24445d199630219d5158555e4
thetimeofblack <1191401005@qq.com>
2019-05-27 19:20:52 +0800

    LeetCode24

---
+++ LeetCode24.java
@@ -0,0 +1,61 @@
+class LeetCode24{
+	public static ListNode swapPairs(ListNode head) {
+        ListNode result = head ;
+		ListNode cur = head; 
+		while(cur!=null){
+			if(cur.next == null) break; 
+			if(cur.next.next == null) break; 
+			ListNode headnext  = cur.next; 
+			ListNode headnextnext = cur.next.next; 
+			ListNode tmphead = cur ;
+			tmphead.next = headnextnext; 
+			headnext.next = headnextnext.next; 
+			headnextnext.next = headnext; 
+			cur = headnext; 
+			
+		}
+		return result; 
+    }
+	
+	public static ListNode createNewListNode(int[] nums){
+		if(nums.length<1) return null; 
+		ListNode head = new ListNode(nums[0]);
+		ListNode cur = head;  
+		for(int i= 1 ;i<nums.length;i++){
+			ListNode next = new ListNode(nums[i]); 
+			cur.next = next;
+			cur = cur.next; 
+		}	
+		return head; 
+	}
+	
+	public static void printListNode(ListNode head){
+		if(head == null) {
+			System.out.println("The ListNode is empty"); 
+			return ;
+		}
+		if(head.next == null) {
+			System.out.println("The ListNode is a single node, the value is "+head.val);
+			return ;
+		}
+		System.out.println("The value is listed as follows: "); 
+		while(head.next!=null){
+			System.out.print(head.val +" "); 
+			head = head.next; 
+		}
+	}
+	
+	public static void main(String[] args){
+		int[] n1 = {1,2,3,4,5,6,6,8}; 
+		int[] n2 = {2,3,4,5,6,6,9,10}; 
+		ListNode h1 = createNewListNode(n1); 
+		ListNode h2 = createNewListNode(n2);
+		ListNode r1 = swapPairs(h1); 
+		ListNode r2 = swapPairs(h2); 
+		printListNode(r1); 
+		printListNode(r2); 
+		
+	}
+	
+	
+}
--- ListNode_Tool.java
+++ ListNode_Tool.java
@@ -1,6 +1,11 @@
 class ListNode_Tool{
-	
-	
+	  private ListNode p ; 
+	  public void setListNode(ListNode n){
+		  this.p =  n ; 
+	  }
+	  public ListNode getListNode(){
+		  return this.p ; 
+	  }
 
 
 }
