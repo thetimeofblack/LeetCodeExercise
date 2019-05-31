2a0194b91c831b5d8eff54fab27e2c28780efe98
thetimeofblack <1191401005@qq.com>
2019-05-31 21:07:17 +0800

    LeetCode25

--- LeetCode25.java
+++ LeetCode25.java
@@ -1,9 +1,52 @@
+/*
+Runtime: 1 ms, faster than 61.04% of Java online submissions for Reverse Nodes in k-Group.
+Memory Usage: 35.8 MB, less than 99.98% of Java online submissions for Reverse Nodes in k-Group.
+*/
 class LeetCode25{
-	public ListNode reverseKGroup(ListNode head, int k) {
+	public static ListNode reverseKGroup(ListNode head, int k) {
         ListNode result = new ListNode(-1);
 		result.next = head ; 
 		ListNode start = result; 
-		while()
+		ListNode Node = head; 
+		boolean isfirstGroup = true; 
+		boolean midend = false ; 
+		while(Node!=null){
+			ListNode nextNode = Node; 
+			ListNode firstNode = Node ; 
+			ListNode preNode = start; 
+			
+			for(int i = 0 ;i<k ;i++){
+			
+			nextNode = Node.next; 
+			Node.next = preNode; 
+			preNode = Node; 
+			Node = nextNode; 
+			if(Node==null&&i!=k-1) {
+				midend =true; 
+				Node = preNode ;
+				preNode = preNode.next; 
+				ListNode prepreNode = preNode.next; 
+				Node.next = null ; 
+				while(i>=0){
+					i--; 
+					preNode.next= Node ; 
+					Node = preNode; 
+					preNode = prepreNode; 
+					prepreNode = prepreNode.next ; 
+				}
+				break;
+			} 
+			}
+			if(midend) break; 
+			if(isfirstGroup){
+				result.next  = preNode ; 
+				isfirstGroup = false; 
+			}
+			firstNode.next=Node; 
+			start.next = preNode; 
+			start = firstNode; 
+			
+		}
 		return result.next; 
     }
 	
@@ -41,8 +84,8 @@ class LeetCode25{
 		int[] n2 = {2,3,4,5,6,6,9,10}; 
 		ListNode h1 = createNewListNode(n1); 
 		ListNode h2 = createNewListNode(n2);
-		ListNode r1 = swapPairs(h1); 
-		ListNode r2 = swapPairs(h2); 
+		ListNode r1 = reverseKGroup(h1,3); 
+		ListNode r2 = reverseKGroup(h2,4); 
 		printListNode(r1); 
 		printListNode(r2); 
 		
